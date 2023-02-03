05a960188e72bad6c70752136355e9963c452b84
xKosuke <113640887+xKosuke@users.noreply.github.com>
2023-02-03 18:02:03 +0100

    Update obf.py

--- obf.py
+++ obf.py
@@ -5,25 +5,25 @@ import argparse
 try:
     from Cython.Build import cythonize
 except ImportError:
-    print("Vous devez installer la bibliothèque cython pour utiliser ce script.")
+    print("Bibliothèque cython non trouvé.")
     sys.exit(1)
 
 parser = argparse.ArgumentParser(description='Obfuscateur de fichier Python avec Cython.')
 parser.add_argument('fichier', metavar='F', type=str, help='Le fichier à obfusquer.')
-parser.add_argument('-o', '--output', type=str, help='Le nom du fichier obfusqué (par défaut, le nom sera "obfusque_fichier.py").', default=None)
+parser.add_argument('-o', '--output', type=str, help='Le nom du fichier obfusqué (par défaut : "obfusque_fichier.py").', default=None)
 
 args = parser.parse_args()
 
-nom_du_fichier = args.fichier
-if not os.path.exists(nom_du_fichier):
-    print("Le fichier %s n'existe pas." % nom_du_fichier)
+nom_du_fichier_pk = args.fichier
+if not os.path.exists(nom_du_fichier_pk):
+    print("Le fichier %s n'existe pas." % nom_du_fichier_pk)
     sys.exit(1)
 
 if args.output:
-    nom_fichier_obfusque = args.output
+    nom_fichier_obfusque_srx = args.output
 else:
-    nom_fichier_obfusque = "obfusque_" + nom_du_fichier
+    nom_fichier_obfusque_srx = "obfusque_" + nom_du_fichier_pk
 
-cythonize(nom_du_fichier, compiler_directives={'linetrace': False, 'binding': True},
-          output_file=nom_fichier_obfusque)
-print("Le fichier %s a été obfusqué avec succès %s." % (nom_du_fichier, nom_fichier_obfusque))
+cythonize(nom_du_fichier_pk, compiler_directives={'linetrace': False, 'binding': True},
+          output_file=nom_fichier_obfusque_srx)
+print("Le fichier %s a été obfusqué avec succès %s." % (nom_du_fichier_pk, nom_fichier_obfusque_srx))
